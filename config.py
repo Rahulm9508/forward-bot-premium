cb6dd1b4eec506f4a455627d4b8092119c1430b2
Rahulm9508 <153660317+Rahulm9508@users.noreply.github.com>
2024-09-29 17:40:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,11 +6,11 @@
 import os
 
 class Config:
-    API_ID = os.environ.get("API_ID", "")
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID = os.environ.get("API_ID", "20346550")
+    API_HASH = os.environ.get("API_HASH", "bc79c3bea7a626887bdc0871eecf0327")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7289136166:AAGPpHK4yz99XD5x6JJixPyTlHZvjlFC5aI") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot-premium") 
-    DB_URL = os.environ.get("DB_URL", "")
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://daxxop:daxxop@daxxop.dg3umlc.mongodb.net/?retryWrites=true&w=majority")
     DB_NAME = os.environ.get("DB_NAME", "Stubborn")
     OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '').split()]
 
